812390ed0b35b79909cc6131461adc430c4ef9d0
1ilsang <1ilsang@naver.com>
2023-06-05 22:44:37 +0900

    feat(app): Add splash timeout

--- App.js
+++ App.js
@@ -1,49 +1,22 @@
 import { SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { useAtom } from "jotai";
 import { NavigationContainer } from "@react-navigation/native";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { Host } from "react-native-portalize";
 import { RootSiblingParent } from "react-native-root-siblings";
+import * as SplashScreen from "expo-splash-screen";
 
 import MainStack from "./src/features/stackNavigation/MainStack";
-import { useLayoutEffect } from "react";
-import { appEnvAtom, folderListAtom, initialApp } from "./src/shared/atoms";
-import { load, LOCAL_STORAGE_KEY, save } from "./src/shared/utils/localStorage";
-import { colorSets } from "./src/shared/constants/colors";
-import { FOLDER_SORT } from "./src/shared/constants/folder";
+import useApp from "./src/features/app/hooks";
+
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
-  const [, setAppEnv] = useAtom(appEnvAtom);
-  const [, setFolderList] = useAtom(folderListAtom);
+  const { appIsReady } = useApp();
 
-  useLayoutEffect(() => {
-    const loadLocalStorage = async () => {
-      const data = await load(LOCAL_STORAGE_KEY.folderList);
-      const appData = await load(LOCAL_STORAGE_KEY.appEnv);
-      const selectedIndex = Math.floor((Math.random() * 10) % 7);
-      const initData =
-        data && data.length > 0
-          ? data
-          : [
-              {
-                id: Number(new Date()),
-                title: "기본 폴더",
-                description: "기본 폴더에요.",
-                defaultFolder: true,
-                icon: "Heart",
-                sort: FOLDER_SORT.DATE,
-                color: colorSets[selectedIndex],
-                linkList: [],
-              },
-            ];
-      const initAppEnv = appData ? appData : initialApp;
-      setFolderList(initData);
-      setAppEnv(initAppEnv);
-      await save(LOCAL_STORAGE_KEY.folderList, initData);
-    };
-    loadLocalStorage();
-  }, []);
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
     <ActionSheetProvider>
---
+++ src/features/app/hooks.jsx
@@ -0,0 +1,62 @@
+import { useAtom } from "jotai";
+import { appEnvAtom, folderListAtom, initialApp } from "../../shared/atoms";
+import { useEffect, useLayoutEffect, useState } from "react";
+import { LOCAL_STORAGE_KEY, load, save } from "../../shared/utils/localStorage";
+import { FOLDER_SORT } from "../../shared/constants/folder";
+import { colorSets } from "../../shared/constants/colors";
+import * as SplashScreen from "expo-splash-screen";
+
+const useApp = () => {
+  const [, setAppEnv] = useAtom(appEnvAtom);
+  const [, setFolderList] = useAtom(folderListAtom);
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useLayoutEffect(() => {
+    const loadLocalStorage = async () => {
+      const data = await load(LOCAL_STORAGE_KEY.folderList);
+      const appData = await load(LOCAL_STORAGE_KEY.appEnv);
+      const selectedIndex = Math.floor((Math.random() * 10) % 7);
+      const initData =
+        data && data.length > 0
+          ? data
+          : [
+              {
+                id: Number(new Date()),
+                title: "기본 폴더",
+                description: "기본 폴더에요.",
+                defaultFolder: true,
+                icon: "Heart",
+                sort: FOLDER_SORT.DATE,
+                color: colorSets[selectedIndex],
+                linkList: [],
+              },
+            ];
+      const initAppEnv = appData ? appData : initialApp;
+      setFolderList(initData);
+      setAppEnv(initAppEnv);
+      await save(LOCAL_STORAGE_KEY.folderList, initData);
+    };
+    loadLocalStorage();
+  }, []);
+
+  useEffect(() => {
+    async function waitSplash() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+    waitSplash();
+  }, []);
+
+  return {
+    appIsReady,
+  };
+};
+
+export default useApp;
