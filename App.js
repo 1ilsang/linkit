19a4a2ce6e487fa0ca02e54f1d5afbbb64ee6488
anyl92 <yulim9592@gmail.com>
2023-09-04 15:54:49 +0900

    feat: delete existing toast library, delete unused import

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
